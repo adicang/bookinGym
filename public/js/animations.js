ec7fc503ab2a89b49c082eeb1a0e95d65ae2b24a
adicang123 <adic@bluerbn.com>
2019-03-07 15:41:06 +0200

    7.3.19

---
+++ public/js/animations.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar',{duration:2000,
+ origin:'bottom'
+    });
+ sr.reveal('#home',{duration:1000,
+ origin:'top',
+    distance:'300px'
+    });
+sr.reveal('.home_h1',{duration:1000,
+ origin:'bottom',
+    distance:'300px'
+    });
+sr.reveal('.home_h3',{duration:1000,
+ origin:'bottom',
+    distance:'300px'
+    });
+sr.reveal('.container-fluid',{duration:2000,
+     origin:'bottom',
+    distance:'300px',
+    viewFactor: 0.2
+    });
+sr.reveal('.card',
+    {duration:2000,
+     origin:'bottom',
+    distance:'300px',
+    viewFactor: 0.2
+    });
+sr.reveal('.FirstCo',{duration:2000,
+     origin:'bottom',
+    distance:'300px',
+    viewFactor: 0.2
+    });
---
+++ public/map_data.php
@@ -0,0 +1,53 @@
+<?php
+
+require("config.php");
+require("database.php");
+
+// Start XML file, create parent node
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+
+
+
+// Select all the rows in the markers table
+
+$sql = "SELECT * FROM Gyms";
+$result=$database->query($sql);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo "<?xml version='1.0' ?>";
+echo '<markers>';
+$ind=0;
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysqli_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'id="' . $row['id'] . '" ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo 'logo="' .$row['logo'] . '" ';
+  echo '/>';
+  $ind = $ind + 1;
+}
+
+// End XML file
+echo '</markers>';
+
+?>
