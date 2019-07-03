b1417ac6d4eb1e89d12aeea52877cfda17b55669
Adi Cang <48086662+adicang@users.noreply.github.com>
2019-07-03 14:58:12 +0300

    Update addGym5.php

--- public/addGym5.php
+++ public/addGym5.php
@@ -246,7 +246,7 @@
 
 
   <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB82EdqJSv80J9--zaL2APp17ybPYlJGc4&libraries=places,geometry&callback=initAutocomplete&language=iw&region=IL"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB82EdqJS456789v80J9--zaL20977ybPYlJGc4&libraries=places,geometry&callback=initAutocomplete&language=iw&region=IL"
     async defer></script>
   <script>
 
