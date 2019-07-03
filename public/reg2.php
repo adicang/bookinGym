069730e3e6db86abccfa87380e4d30a156ce722b
Adi Cang <48086662+adicang@users.noreply.github.com>
2019-07-03 15:04:05 +0300

    Update reg2.php

--- public/reg2.php
+++ public/reg2.php
@@ -143,7 +143,7 @@
 
   
   <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB82EdqJSv80J9--zaL2APp17ybPYlJGc4&libraries=places,geometry&callback=initAutocomplete&language=iw&region=IL"
+    src="https://maps.googleapis.com/maps/api/js?key=,geometry&callback=initAutocomplete&language=iw&region=IL"
     async defer></script>
   
   
