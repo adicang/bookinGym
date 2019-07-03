481eae4f360af4c59e546544f6552ade9e5167f0
Adi Cang <48086662+adicang@users.noreply.github.com>
2019-07-03 15:04:26 +0300

    Update regSuccess.php

--- public/regSuccess.php
+++ public/regSuccess.php
@@ -133,7 +133,7 @@
 
 
     <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB82EdqJSv80J9--zaL2APp17ybPYlJGc4&libraries=places,geometry&callback=initAutocomplete&language=iw&region=IL"
+        src="https://maps.googleapis.com/maps/api/js?eometry&callback=initAutocomplete&language=iw&region=IL"
         async defer></script>
 
 
